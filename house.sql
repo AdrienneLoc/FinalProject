d97b13dd213a634214ab93b9dc63c80376d51d19
Adrienne Loc <al738931@sju.edu>
2023-11-23 18:57:57 -0500

    switching utilities and adding basement

--- house.sql
+++ house.sql
@@ -6,19 +6,13 @@ create table house (
     bedrooms      integer       check(bedrooms > 0), -- number of bedrooms
     bathrooms     integer       check(bathrooms > 0), -- number of bathrooms
     parking       varchar(5)    check(parking in('yes', 'no')), -- is there parking?
-    utilities     varchar(20)   check(utilities in('tenant', 'landlord')), -- who pays?
+    basement     varchar(20)   check(basement in('finished', 'unfinished', 'n/a')), -- is there a basement and is it done?
     address       text          not null,
     postal_code   integer       check(postal_code > 19004 and postal_code < 19444), -- restricting to Philadeplhia
     primary key(house_id)
 );
 
 -- inserting sample values into table
-insert into house values(1234, 2, 'Townhouse', 2, 2, 'yes', 'landlord', '1111 Market Street, Philadelphia, PA', 19103);
-insert into house values(5678, 2, 'Condo', 3, 3, 'yes', 'tenant', '2222 Aldine Street, Philadelphia, PA', 19136);
-insert into house values(9102, 3, 'Duplex', 2, 1, 'no', 'landlord', '3333 Cabell Road, Philadelphia, PA', 19154);
-
-
-
-
-
-
+insert into house values(1234, 2, 'Townhouse', 2, 2, 'yes', 'finished', '1111 Market Street, Philadelphia, PA', 19103);
+insert into house values(5678, 2, 'Condo', 3, 3, 'yes', 'finished', '2222 Aldine Street, Philadelphia, PA', 19136);
+insert into house values(9102, 3, 'Duplex', 2, 1, 'no', 'unfinished', '3333 Cabell Road, Philadelphia, PA', 19154);
--- listing.sql
+++ listing.sql
@@ -6,6 +6,7 @@ create table listing (
     description     text            not null,
     date_listed     date            check(date_listed between date('2000-01-01') and sysdate()), -- checking for valid date
     rent            decimal(10,2)   check(rent > 0), 
+    utilities       varchar(20)     check(utilities in('tenant', 'landlord')), -- who pays?
     date_available  date            not null, 
     primary key(listing_id),
     foreign key(agent_id) references agent(agent_id),
@@ -22,16 +23,16 @@ insert into listing values(8217, 123456, 1234, 'Beautiful 2 bedroom, 2 bath end-
                                                 The master bedroom has new hardwood floors, is also located on the main floor and has a large walk-in closet with custom shelving. 
                                                 The 1st fl bedroom includes a built-in shelving unit, plenty of storage, and a private full bathroom. Each level has its own luxurious, full bathroom with granite vanities. 
                                                 Additional features include CENTRAL AIR, lots of storage, and side garden! The garage has a built-in storage unit and second refrigerator for extra storage.',
-                                                "2023-09-06", 2456.00, "2024-01-01");
+                                                "2023-09-06", 2456.00, 'landlord', "2024-01-01");
 insert into listing values(3548, 123456, 5678, 'Welcome home to this brand new bi-level condo on Aldine! This condo is perfectly positioned to access all of the great restaurants and retail shops in the area! 
                                                 Enter unit A to a stunning open-concept living area that leads into a wrap-around kitchen equipped with quartz counters, stainless steel appliances and marble backsplash. 
                                                 Bedrooms on both floors offer tons of space and have direct access to a private bath.',
-                                                '2023-10-24', 3789.00, '2023-11-07');
+                                                '2023-10-24', 3789.00, 'tenant', '2023-11-07');
 insert into listing values(1759, 123456, 9102, '2 Bedroom 1 Bath complete remodel. Updated kitchen: center island, New cabinets, New appliances, Granite counter tops. 
 												Great views of the backyard from the kitchen and breakfast room windows! Remodel and new appliances 2023. Open concept. 
                                                 Vaulted Living area. Tall baseboards through out, and New Luxury Vinyl plank flooring. New windows to help keep your utility bills down, hot water heater. 
                                                 HVAC recently services and new AC outside unit (condenser) installed. Large backyard. Close to Six Flags, Hurricane Harbor, Globe Life Field, AT&T stadium, and more! Great Location!',
-                                                '2023-11-14', 3789.00, '2023-11-18')
+                                                '2023-11-14', 3789.00, 'landlord', '2023-11-18')
                                                 
                                                 
 /* LINKS FOR DESCRIPTIONS ARE LOCATED IN THE LISTING EXCEL FILE */
